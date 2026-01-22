d6f566439384a6c5107366e03f801286dfe84709
vidushi-singh11 <vidushisingh0705@gmail.com>
2026-01-22 21:06:32 +0530

    solution done

--- bearnbrother.java
+++ bearnbrother.java
@@ -1,15 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int count=0;
-        while(a<=b){
-            a=a*3;
-            b=b*2;
-            count++;
-        }
-        System.out.println(count);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int result = (m * n) / 2;
+        System.out.println(result);
     }
 }
