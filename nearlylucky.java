b87f7a80a8ad4820df8f530f634578912766c70e
vidushi-singh11 <vidushisingh0705@gmail.com>
2026-01-26 23:45:24 +0530

    solution added

---
+++ nearlylucky.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m=0;
+        int count=0;
+        while(n!=0){
+            
+            m=(n%10);
+            if(m==4 || m==7){
+                count++;
+            }
+            n=n/10;
+        }
+        if (count==4 || count==7){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
